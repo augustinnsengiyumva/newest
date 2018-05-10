482266952d859026f81935b35f0041d147c1ce75
Augustin NSENGIYUMVA <nsau2020@gmail.com>
2018-05-10 17:40:29 +0000

    init

---
+++ app/controllers/blogs_controller.rb
@@ -0,0 +1,64 @@
+class BlogsController < ApplicationController
+  before_action :set_blog, only: [:show, :edit, :update, :destroy] 
+  
+ 
+  
+  def index
+    @blogs = Blog.all
+  end
+
+  # Add
+  def new
+  @blogs = Blog.new
+  end
+  def create
+    @blog = Blog.new(blog_params)
+    #@blog.user_id = current_user.id
+    if @blog.save
+      # Switch to the list screen and display a message saying "You have created new blog!"
+      redirect_to blogs_path, Notice: "You have created new blog!"
+    else
+      # Redraw the input form.
+      render 'new'
+    end
+  end
+  def show
+    # 追記する
+     @blogs = Blog.find(params[:id])
+  end
+  def edit
+     @blogs = Blog.find(params[:id])
+  end
+    
+    def update
+    @blog = Blog.find(params[:id])
+    if @blog.update(blog_params)
+      redirect_to blogs_path, notice: "You have edited this blog！"
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    @blog.destroy
+    redirect_to blogs_path, Notice: "You have deleted the blog!"
+  end
+  
+  
+
+  #Omitted
+  private
+  #Before_action : Set_blog, only: [:show, :edit, :update, :destroy] 
+   def blog_params
+    params.require(:blog).permit(:title, :content)
+  end
+  
+  def set_blog
+    @blog = Blog.find(params[:id])
+  end
+end  
+  #Omission
+  
+  #Omission
+  #Omitted
+
---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+
+  get 'sessions/new'
+
+ resources :blogs
+ post 'blogs/new'
+ get 'blogs/index'
+ resources :users, only: [:new, :create, :show]
+ resources :blogs, only: [:new, :create, :edit, :destroy]
+ resources :sessions, only: [:new, :create, :edit, :destroy]
+  resources :users
+  resources :blogs
+  
+  root to: 'sessions#new'
+ 
+ # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
